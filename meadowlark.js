4963472fb2763e1667330cd55fcbd88b2c641f94
Robbie Jaeger <jaeger.rob@gmail.com>
2016-10-16 17:32:32 -0600

    Add layout template and views

--- meadowlark.js
+++ meadowlark.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 
 // set up handlebars view engine
-var handlebars = require('express-handlebars')
-  .create({defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
